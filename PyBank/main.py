090fd2641583f66f6ac7ef645608fc54263728b0
Michael Blackmon <differentiablef@gmail.com>
2019-06-30 07:08:34 -0400

    finalized PyBank challenge solution

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 
 # imports ######################################################################
 
-import os, csv
+import sys, os, csv
 
 # variables ####################################################################
 
@@ -11,9 +11,9 @@ months = [ ]
 values = [ ]
 deltas = [ ]
 
-# routines #####################################################################
+# functions ####################################################################
 
-def extract_data():
+def extract_data(header=True):
     """Process contents of CSV file 'data_path', populating the lists
        'months', 'values', and 'deltas' with the relevent information (in order)"""
     
@@ -21,8 +21,9 @@ def extract_data():
         # create reader
         csvobj = csv.reader(infile, delimiter=',')
 
-        # burn header
-        next(csvobj)
+        # burn header, if present
+        if header:
+            next(csvobj)
 
         # process remaining entries 
         for entry in csvobj:
@@ -37,12 +38,38 @@ def extract_data():
             months.append(month)            
     return
 
+
+
 def print_summary():
     """print summary info. for data extracted using 'extract_data' method"""
+
+    # extract largest total *decrease* in profit by
+    #    finding minimum value of negative changes to profit
+    max_dec = min(filter(lambda x : x < 0, deltas))
+    max_dec_month = months[ deltas.index(max_dec) + 1 ]
+
+    # extract largest total *increase* in profit by
+    #    finding maximum value of postitive changes to profit
+    max_inc = max(filter(lambda x : x > 0, deltas))
+    max_inc_month = months[ deltas.index(max_inc) + 1 ]
+    
+    print("Financial Summary/Analysis\n----------------------------------------\n"
+          " Months:\n   {:20s}:{:>15d}\n\n"
+          " Profit/Loss:\n   {:20s}: ${:13.2f}\n   {:20s}: ${:13.2f}\n\n"
+          " Extrema:\n   {:20s}: ${:13.2f}\n   {:20s}:{:>15s}\n"
+          "   {:20s}: ${:13.2f}\n"
+          "   {:20s}:{:>15s}\n".format("Total", len(months),
+                                        "Total", sum(values),
+                                        "Average Change", sum(deltas)/len(deltas),
+                                        "Max. Increase", max_inc,
+                                        "Max. Increase Month", max_inc_month,
+                                        "Min. Decrease", max_dec,
+                                        "Min. Decrease Month", max_dec_month))
     return
 
 # script entry-point ###########################################################
 
 if __name__ == "__main__":
-    print("boo")
-    
+    extract_data()   # extract data from 'budget_data.csv'
+    print_summary()  # produce summary for extracted data
+    sys.exit(0)      # exit cleanly
