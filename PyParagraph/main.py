6e99378e2ef9d5a954d0d117b9f2b17f7ef7790a
Michael Blackmon <differentiablef@gmail.com>
2019-07-06 04:31:51 -0400

    added comments

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -9,16 +9,19 @@ from nltk import download
 # script #######################################################################
 
 data_dir = os.path.join('.', 'Resources')
-sep = '-' * 50
-summary_fmt = """
-{sep}
+
+summary_fmt = """{sep}
 Filename:             {path}
 Sentence Count:       {sent_count}
 Mean Sentence Length: {mean_sent_length}
 Word Count:           {word_count}
 Mean Word Length:     {mean_word_length}"""
 
-# initialize list of texts to process
+sep =  ('-' * 50)
+
+# script #######################################################################
+
+# initialize list of files to process
 data_paths = list()
 for filename in os.listdir(data_dir):
     data_paths.append(
@@ -28,15 +31,18 @@ for filename in os.listdir(data_dir):
 download('punkt')
 
 # process the prose contained in each file listed in 'data_paths'
-#   printing a summary...
 for path in data_paths:
     with open(path, newline='') as infile:
         prose = infile.read()
-
+        pass
+    
+    # extract list of tokens using 'nltk.tokenize.word_tokenize'
     tokens = word_tokenize( prose )
-    parts = filter(lambda t:\
-                        re.fullmatch('[^.\w]+', t) == None, tokens)
 
+    # filter out non-essential punct. 
+    parts = filter(lambda t: re.fullmatch('[^.\w]+', t) == None, tokens)
+
+    # simultaneously compute the relevant summary values.
     word_count = 0
     mean_word_length = 0.0
     sent_count = 0
@@ -56,9 +62,13 @@ for path in data_paths:
     mean_word_length = mean_word_length/word_count
     mean_sent_length = mean_sent_length/sent_count
 
+    # display summary information for contents of 'path'
     print(summary_fmt.format(sep=sep,
                              word_count=word_count,
                              mean_word_length=mean_word_length,
                              sent_count=sent_count,
                              mean_sent_length=mean_sent_length,
                              path=path))
+    pass
+
+print(f'{sep}')
